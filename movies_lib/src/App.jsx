045d07ddde9302b8046eabd81ed73f313886cb44
Michael <michaelpetterson06.com.br>
2022-07-10 16:45:33 -0300

    Criando e estruturando component NavBar

--- movies_lib/src/App.jsx
+++ movies_lib/src/App.jsx
@@ -1,11 +1,15 @@
 import './App.css'
+import { Link, Outlet } from 'react-router-dom';
+import NavBar from './components/NavBar';
 
 function App() {
 
 
   return (
     <div className="App">
+      <NavBar />    
       <h2>Movies lib</h2>
+      <Outlet/>
      </div>
   )
 }
---
+++ movies_lib/src/components/NavBar.jsx
@@ -0,0 +1,21 @@
+import { Link } from 'react-router-dom';
+import { BiCameraMovie, BiSearchAlt2 } from 'react-icons/bi';
+
+function NavBar () {
+    return (
+        <nav id='navbar'>
+            <h2>
+                <Link to='/'><BiCameraMovie />Movieslib</Link>
+            </h2>
+            <form>
+                <input type='text' placeholder='Busque um filme' />
+                <button type='submit'>
+                    <BiSearchAlt2 />
+                </button>
+            </form>
+            
+        </nav>    
+    )
+}
+
+export default NavBar;
