e665b41d3246bca526d48eeb1d31b17045e990b9
Michael <michaelpetterson06.com.br>
2022-07-10 21:04:19 -0300

    estruturando component Home e chamando API

--- movies_lib/src/pages/Home.jsx
+++ movies_lib/src/pages/Home.jsx
@@ -1,4 +1,26 @@
+import { useState, useEffect } from 'react';
+
+const moviesURL = import.meta.env.VITE_API;
+const apiKey = import.meta.env.VITE_API_KEY;
+
 function Home () {
+
+    const [ topMovies, setTopMovies ] = useState([])        
+
+    useEffect(() => {
+        const toRateUrl = `${moviesURL}top_rated?${apiKey}`
+        getTopRateMovies(toRateUrl);
+    },[])
+
+    
+    const getTopRateMovies = async (url) => {
+        const res = await fetch(url);
+        const data =await res.json();
+        setTopMovies(data.results)
+    }
+
+    console.log(topMovies)
+    
     return (
         <div>Home</div>
     )
