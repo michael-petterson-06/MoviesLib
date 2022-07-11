7a0287cb8f560e1adc36a61f5604ab356f0c570c
Michael <michaelpetterson06.com.br>
2022-07-10 23:45:28 -0300

    evento de buscar por filmes

--- movies_lib/src/pages/Search.jsx
+++ movies_lib/src/pages/Search.jsx
@@ -1,7 +1,41 @@
-function Search () {
-    return (
+import { useState, useEffect } from 'react';
+import { useSearchParams } from 'react-router-dom';
+import MovieCard from '../components/MovieCard';
+import './MoviesGrid.css';
 
-        <h1>Search</h1>
+
+const searchURL = import.meta.env.VITE_SEARCH;
+const apiKey = import.meta.env.VITE_API_KEY;
+
+function Search () {
+
+    const [searchParams] = useSearchParams();
+
+    const [movies, setMovies] = useState([]);
+    const query = searchParams.get("q");
+  
+    const getSearchedMovies = async (url) => {
+        const res = await fetch(url);
+        const data = await res.json();
+        setMovies(data.results);
+    };
+  
+    useEffect(() => {
+        const searchWithQueryURL = `${searchURL}?${apiKey}&query=${query}`;
+        getSearchedMovies(searchWithQueryURL);
+    }, [query]);
+
+
+    return (
+        <div className="container">
+            <h2 className="title">
+            Resultados para: <span className="query-text">{query}</span>
+            </h2>
+        <div className="movies-container">
+            {movies.length > 0 &&
+            movies.map((movie) => <MovieCard key={movie.id} movie={movie} />)}
+        </div>
+      </div>
     )
      
 }
